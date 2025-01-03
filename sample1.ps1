4749548ba7fadf8e049addc887cdce84c1f2b918
Ehsan Haghpanah <anonim.developer@gmail.com>
2025-01-03 13:36:32 +0330

    beginning of the universe!

---
+++ sample1.ps1
@@ -0,0 +1,12 @@
+﻿
+# define the target folder and output file
+$target = "G:\"
+$output = "I:\books.txt"
+
+# get files in target folder and its subfolders
+$files = Get-ChildItem -Path $target -File -Recurse
+
+# Save the list of files to the output file
+$files.FullName | Out-File -FilePath $output -Encoding UTF8
+
+Write-Host "list saved to $output"
