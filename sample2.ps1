0c6d6559bd867f6f11ad5d80c837bc35f8c4d985
Ehsan Haghpanah <anonim.developer@gmail.com>
2025-01-03 13:46:03 +0330

    .

---
+++ sample2.ps1
@@ -0,0 +1,24 @@
+﻿function Get-FilesInFolder
+{
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [string] $SearchPath
+    )
+
+    # checks if search path exists
+    if (-not (Test-Path -Path $SearchPath))
+    {
+        Write-Error "The folder path '$SearchPath' does not exist."
+        return
+    }
+
+    # gets all files in search path and its subfolders
+    $files = Get-ChildItem -Path $SearchPath -File -Recurse
+
+    # returns only files' names
+    return $files.Name
+}
+
+$list = Get-FilesInFolder -SearchPath "G:\"
+$list | ForEach-Object { Write-Host $_ }
